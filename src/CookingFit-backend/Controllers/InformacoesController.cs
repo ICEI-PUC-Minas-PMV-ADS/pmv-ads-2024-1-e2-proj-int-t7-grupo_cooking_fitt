087bf31ec7072d965956f0e96f49b21b3dd867ca
JOAO VICTOR DINIZ PIAZZA <144743654+JoaoPiazza07@users.noreply.github.com>
2024-05-15 15:37:54 -0300

    Informação usuario

---
+++ src/CookingFit-backend/Controllers/InformacoesController.cs
@@ -0,0 +1,105 @@
+﻿using CookingFit_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookingFit_backend.Controllers
+{
+    public class InformacoesController : Controller
+    {
+        private readonly InformacaoThumbnailContext _context;
+        public InformacoesController(InformacaoThumbnailContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var dados = await _context.Informacoes.ToListAsync();
+
+            return View(dados);
+        }
+
+        public IActionResult Create() //Exibe um formulário
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Informacao informacao) //Add algo e salva no banco de dados
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Informacoes.Add(informacao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(informacao);
+        }
+        public async Task<IActionResult> Edit(int? IdUsuario)
+        {
+            if (IdUsuario == null)
+                return NotFound();
+
+            var dados = await _context.Informacoes.FindAsync(IdUsuario);
+
+            if (dados == null)
+                return NotFound();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int IdUsuario, Informacao informacao)
+        {
+            if (IdUsuario == informacao.IdUsuario)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Informacoes.Update(informacao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public async Task<IActionResult> Details(int? IdUsuario)
+        {
+            if (IdUsuario == null)
+                return NotFound();
+            var dados = await _context.Informacoes.FindAsync(IdUsuario);
+            if (dados == null)
+                return NotFound();
+            return View(dados);
+        }
+
+
+        public async Task<IActionResult> Delete(int? IdUsuario)
+        {
+            if (IdUsuario == null)
+                return NotFound();
+            var dados = await _context.Informacoes.FindAsync(IdUsuario);
+            if (dados == null)
+                return NotFound();
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? IdUsuario)
+        {
+            if (IdUsuario == null)
+                return NotFound();
+            var dados = await _context.Informacoes.FindAsync(IdUsuario);
+            if (dados == null)
+                return NotFound();
+            _context.Informacoes.Remove(dados);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/CookingFit-backend/Models/InformacaoThumbnailContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CookingFit_backend.Models
+{
+    public class InformacaoThumbnailContext : DbContext
+    {
+        public InformacaoThumbnailContext(DbContextOptions<InformacaoThumbnailContext> options) : base(options) { }
+
+        public DbSet<Informacao> Informacoes { get; set; }
+        
+    }
+}
