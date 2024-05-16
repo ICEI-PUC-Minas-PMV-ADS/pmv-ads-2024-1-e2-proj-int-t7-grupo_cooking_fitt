8428a578bd8f09127a2ec6705c630d79d583f285
Emilia-ads <144388125+Emilia-ads@users.noreply.github.com>
2024-05-16 18:24:19 -0300

    Delete src directory

--- src/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CookingFit_backend.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
