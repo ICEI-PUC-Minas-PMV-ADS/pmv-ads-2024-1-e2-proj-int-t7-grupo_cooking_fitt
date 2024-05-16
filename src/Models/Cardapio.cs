0f82adf7e1c00a3f2c2b817cda4ff39832d617ec
Emilia-ads <144388125+Emilia-ads@users.noreply.github.com>
2024-05-16 17:34:13 -0300

    Arquivos acessorios

---
+++ src/Models/Cardapio.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CookingFit_backend.Models
+{
+    [Table("Cardápio")]
+    public class Cardapio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório, informar a descricao!")]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório, informar o total de calorias!")]
+        public int Calorias { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório, informar a quantidade! (Ex.: 1 colher, meia chícara, etc.)")]
+        public string Quantidade { get; set; }
+
+    }
+}
