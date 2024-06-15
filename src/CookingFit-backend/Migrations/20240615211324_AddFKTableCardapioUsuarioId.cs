3a24ca1bd99f729e74abadb5ce5ff475f278de8c
Emilia-ads <144388125+Emilia-ads@users.noreply.github.com>
2024-06-15 18:49:18 -0300

    Add FK ItemCardapio

---
+++ src/CookingFit-backend/Migrations/20240615211324_AddFKTableCardapioUsuarioId.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookingFit_backend.Migrations
+{
+    public partial class AddFKTableCardapioUsuarioId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId_Cardapio",
+                table: "Cardapio",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cardapio_UsuarioId_Cardapio",
+                table: "Cardapio",
+                column: "UsuarioId_Cardapio");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cardapio_Usuario_UsuarioId_Cardapio",
+                table: "Cardapio",
+                column: "UsuarioId_Cardapio",
+                principalTable: "Usuario",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cardapio_Usuario_UsuarioId_Cardapio",
+                table: "Cardapio");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cardapio_UsuarioId_Cardapio",
+                table: "Cardapio");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId_Cardapio",
+                table: "Cardapio");
+        }
+    }
+}
